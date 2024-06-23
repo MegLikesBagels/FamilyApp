b0e5dc829fe04248bae1cf7c4487d7db3785da5e
Megan Barrett-Pender <meganbarrettpender03+developer@gmail.com>
2024-06-23 08:18:50 +0100

    navbar with a animated underline

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const http = require("http");
-const fs =  require("fs/promises");
+const fs = require("fs/promises");
 const port = 5043;
 const host = "localhost";
 
 const requestListener = function (req, res) {
-
-    switch (req.url) {
+    switch (decodeURI(req.url)) {
         case "/":
             doReadFile("index.html");
+            break;
+        case "/fonts.css":
+            doReadFile("fonts.css", "text/css");
             break;
         case "/reset.css":
             doReadFile("reset.css", "text/css");
@@ -18,41 +20,40 @@ const requestListener = function (req, res) {
         case "/index.js":
             doReadFile("index.js", "text/javascript");
             break;
+        case "/Megan Logo.png":
+            doReadFile("Megan Logo.png", "image/png");
+            break;
     }
 
-    function prettyConsole ( ...params ) {
-
+    function prettyConsole(...params) {
         let currentDate = new Date();
         let hours = currentDate.getUTCHours();
         let minutes = currentDate.getUTCMinutes();
         let seconds = currentDate.getUTCSeconds();
         let tag = `[${hours}:${minutes}:${seconds}]`;
 
-        return console.log.apply(console, [tag, ...params])
-
+        return console.log.apply(console, [tag, ...params]);
     }
 
-    function doReadFile(readWhat, encoding = "text/html", assetFolder = "public") {
-
-        
-
+    function doReadFile(
+        readWhat,
+        encoding = "text/html",
+        assetFolder = "public"
+    ) {
         fs.readFile(`${__dirname}/${assetFolder}/${readWhat}`)
-        .then(contents => {
-            res.setHeader("Content-Type", encoding);
-            res.writeHead(200);
-            prettyConsole("[200 OK!]", readWhat);
-            res.end(contents);
-        })
-        .catch(err => {
-            res.writeHead(500);
-            prettyConsole("[500 Error!]", err);
-            res.end(err);
-            return;
-        });
-
+            .then((contents) => {
+                res.setHeader("Content-Type", encoding);
+                res.writeHead(200);
+                prettyConsole("[200 OK!]", readWhat);
+                res.end(contents);
+            })
+            .catch((err) => {
+                res.writeHead(500);
+                prettyConsole("[500 Error!]", err);
+                res.end(err);
+                return;
+            });
     }
-
-    
 };
 
 const server = http.createServer(requestListener);
--- public/index.js
+++ public/index.js
@@ -1,2 +1,69 @@
 (function (window) {
-})(window)
+    /* Reference:
+     * | * = classes
+     * | # = id
+     * | > = direct child
+     * | + = the element immediately after
+     * | ~ = any element after
+     * | :nth-child = the X child of type, e.g. li > a:nth-child(4) is the 4th a element directly inside a li element
+     */
+    let navbar = document.querySelector("#navbar"); // gets the navbar element
+    let navLinks = document.querySelectorAll("#navbar > ul > li > a"); // gets all nav link elements inside our navbar element
+    let navVisualAid = document.querySelector("#nav-visual-aid"); // gets the visual aid element
+    let lastNavLinkSelected = null;
+
+    if (navLinks.length <= 0) return; // If Elements are not found, do nothing.
+
+    // For each element in navLinks add the onMouseOver function onNavLinkHover
+    navLinks.forEach(function (navLink) {
+        navLink.addEventListener("mouseover", onNavLinkHover);
+    });
+
+    /**
+     * onNavLinkHover
+     * @param {MouseEvent} event
+     * @returns {void}
+     */
+    function onNavLinkHover(event) {
+        event.stopPropagation(true);
+
+        if (event.target === null) return; // If EventTarget is null, do nothing.
+
+        computeNavVisualAidPosition(event.target);
+
+        return;
+    }
+
+    /**
+     * computeNavVisualAidPosition
+     * @param {HTMLAnchorElement | null} navLink
+     * @returns {void}
+     */
+    function computeNavVisualAidPosition(navLink) {
+        if (navLink === null) return; // If navLink isn't the specified, do nothing;
+
+        let navLinkViewportProperties = navLink.getBoundingClientRect();
+        let navLinkPosition =
+            navLink.parentElement?.parentElement?.offsetLeft +
+            navLink.offsetLeft;
+
+        if (navbar === null) return; // If Element doesn't exist, do nothing
+        if (navVisualAid === null) return; // If Element doesn't exist, do nothing.
+
+        navVisualAid.style.width = `${navLinkViewportProperties.width}px`;
+        navVisualAid.style.left = `${navLinkPosition}px`;
+
+        lastNavLinkSelected = navLink;
+    }
+
+    /**
+     * onScreenResize
+     * @param {Event} event
+     * @returns {void}
+     */
+    function onScreenResize(event) {
+        computeNavVisualAidPosition(lastNavLinkSelected);
+    }
+
+    window.addEventListener("resize", onScreenResize);
+})(window);
